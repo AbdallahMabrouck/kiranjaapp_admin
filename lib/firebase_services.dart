7414fca0c5f4dc9b3e131ab3d88799a037b8a111
AbdallahMabrouck <mabrouck32@gmail.com>
2023-06-16 21:55:04 +0300

    improved category screen and add firebase services

---
+++ lib/firebase_services.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseService {
+  CollectionReference categories =
+      FirebaseFirestore.instance.collection("categories");
+  /* CollectionReference mainCat =
+      FirebaseFirestore.instance.collection("mainCategories");
+  CollectionReference subCat =
+      FirebaseFirestore.instance.collection("subCategories");
+  CollectionReference vendor = FirebaseFirestore.instance.collection("vendor");*/
+
+  Future<void> saveCategory(Map<String, dynamic> data) {
+    return categories.doc(data["name"]).set(data);
+  }
+
+  /* Future<void> saveCategory(Map<String, Object> map,
+      {CollectionReference? reference,
+      Map<String, dynamic>? data,
+      String? docName}) {
+    return reference!.doc(docName).set(data);
+  }  */
+
+  /* updateData(
+      {required Map<String, bool> data,
+      String? docName,
+      required CollectionReference<Object?> reference}) {}
+}
+
+Future<void> updateData(
+    {CollectionReference? reference,
+    Map<String, dynamic>? data,
+    String? docName}) {
+  return reference!.doc(docName).update(data!);*/
+}
--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,5 +1,10 @@
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+
+import '../firebase_services.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -11,6 +16,11 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  final FirebaseStorage storage = FirebaseStorage.instance;
+  final FirebaseService _service = FirebaseService();
+  final TextEditingController _catName = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   dynamic image;
   String? fileName;
 
@@ -28,103 +38,156 @@ class _CategoryScreenState extends State<CategoryScreen> {
     }
   }
 
+  saveImageToDb() async {
+    EasyLoading.show();
+    var ref = firebase_storage.FirebaseStorage.instance
+        .ref("categoryImage/$fileName");
+    try {
+      await ref.putData(image);
+      // image will upload to firebase storage
+      // need to get the download link of that image to save in firestore
+      String downloadURL = await ref.getDownloadURL().then((value) {
+        if (value.isNotEmpty) {
+          // save data to firestore
+          _service.saveCategory({
+            "catName": _catName.text,
+            "image": value,
+            "active": true,
+          }).then((value) {
+            // after save clear all data from the screen
+            clear();
+            EasyLoading.dismiss();
+          });
+        }
+        return value;
+      });
+    } on FirebaseException catch (e) {
+      clear();
+      EasyLoading.dismiss();
+      print(e.toString());
+    }
+  }
+
+  clear() {
+    setState(() {
+      _catName.clear();
+      image = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.all(10),
-          child: const Text(
-            "Category Screen",
-            style: TextStyle(fontSize: 36, fontWeight: FontWeight.w700),
-          ),
-        ),
-        const Divider(
-          color: Colors.grey,
-        ),
-        Row(
-          children: [
-            const SizedBox(
-              width: 10,
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              "Category Screen",
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.w700),
             ),
-            Column(
-              children: [
-                Container(
-                  height: 150,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade500,
-                    borderRadius: BorderRadius.circular(4),
-                    border: Border.all(color: Colors.grey.shade800),
+          ),
+          const Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 150,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade500,
+                      borderRadius: BorderRadius.circular(4),
+                      border: Border.all(color: Colors.grey.shade800),
+                    ),
+                    child: Center(
+                        child: image == null
+                            ? const Text("Category Image")
+                            : Image.memory(image)),
                   ),
-                  child: Center(
-                      child: image == null
-                          ? const Text("Category Image")
-                          : Image.memory(image)),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  onPressed: pickImage,
-                  child: const Text("Upload Image"),
-                ),
-              ],
-            ),
-            const SizedBox(
-              width: 20,
-            ),
-            SizedBox(
-              width: 200,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    label: Text("Enter Category Name"),
-                    contentPadding: EdgeInsets.zero),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: pickImage,
+                    child: const Text("Upload Image"),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            TextButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.white),
-                side: MaterialStateProperty.all(
-                  BorderSide(color: Theme.of(context).primaryColor),
+              const SizedBox(
+                width: 20,
+              ),
+              SizedBox(
+                width: 200,
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Enter Category Name";
+                    }
+                    return null;
+                  },
+                  controller: _catName,
+                  decoration: const InputDecoration(
+                      label: Text("Enter Category Name"),
+                      contentPadding: EdgeInsets.zero),
                 ),
               ),
-              child: Text(
-                "Cancel",
-                style: TextStyle(color: Theme.of(context).primaryColor),
+              const SizedBox(
+                width: 10,
               ),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text(
-                "Save",
+              TextButton(
+                onPressed: clear,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  side: MaterialStateProperty.all(
+                    BorderSide(color: Theme.of(context).primaryColor),
+                  ),
+                ),
+                child: Text(
+                  "Cancel",
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                ),
               ),
-            ),
-          ],
-        ),
-        const Divider(
-          color: Colors.grey,
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.all(10),
-          child: const Text(
-            "Category List",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+              const SizedBox(
+                width: 10,
+              ),
+              image == null
+                  ? Container()
+                  : ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          saveImageToDb();
+                        }
+                      },
+                      child: const Text(
+                        "Save",
+                      ),
+                    ),
+            ],
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-      ],
+          const Divider(
+            color: Colors.grey,
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              "Category List",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
     );
   }
 }
