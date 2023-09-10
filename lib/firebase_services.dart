5e22b34e422874863562ac8740550b10b5c81bfa
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-10 16:26:29 +0300

    made create delivery boy widget

--- lib/firebase_services.dart
+++ lib/firebase_services.dart
@@ -69,6 +69,48 @@ class FirebaseService {
     });
   }
 
+  // Method to show a dialog
+  Future<void> showMyDialog({
+    required BuildContext context,
+    required String title,
+    required String message,
+  }) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Method to show a progress dialog
+  void showProgressDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
+  // Method to dismiss a progress dialog
+  void dismissProgressDialog(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
   // Updated method to show a progress indicator and handle errors
   Future<void> updateBoyStatus({id, context, status}) async {
     try {
--- lib/widgets/deliveryBoy/create_deliveryboy.dart
+++ lib/widgets/deliveryBoy/create_deliveryboy.dart
@@ -3,6 +3,184 @@ import 'package:flutter/material.dart';
 import '../../firebase_services.dart';
 
 class CreateNewBoyWidget extends StatefulWidget {
+  const CreateNewBoyWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CreateNewBoyWidget> createState() => _CreateNewBoyWidgetState();
+}
+
+class _CreateNewBoyWidgetState extends State<CreateNewBoyWidget> {
+  final FirebaseService _services = FirebaseService();
+  var emailText = TextEditingController();
+  var passwordText = TextEditingController();
+
+  bool _visible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      width: MediaQuery.of(context).size.width,
+      height: 80,
+      child: Row(
+        children: [
+          Visibility(
+            visible: _visible ? false : true,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Container(
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _visible = true;
+                    });
+                  },
+                  child: const Text(
+                    "Create New Delivery Boy",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Visibility(
+            visible: _visible,
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Container(
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        height: 30,
+                        child: TextField(
+                          controller: emailText,
+                          decoration: const InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                                width: 1,
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: "Email ID",
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.only(left: 20),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: 200,
+                        height: 30,
+                        child: TextField(
+                          controller: passwordText,
+                          decoration: const InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                                width: 1,
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: "Password",
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.only(left: 20),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (emailText.text.isEmpty) {
+                            _services.showMyDialog(
+                              context: context,
+                              title: "Email ID",
+                              message: "Email ID not entered",
+                            );
+                            return;
+                          }
+
+                          if (passwordText.text.isEmpty) {
+                            _services.showMyDialog(
+                              context: context,
+                              title: "Password",
+                              message: "Password not entered",
+                            );
+                            return;
+                          }
+
+                          if (passwordText.text.length < 6) {
+                            // minimum 6 characters
+                            _services.showMyDialog(
+                              context: context,
+                              title: "Password",
+                              message: "Minimum 6 characters",
+                            );
+                            return;
+                          }
+
+                          _services.showProgressDialog(context);
+
+                          _services
+                              .saveDeliveryBoys(
+                            emailText.text,
+                            passwordText.text,
+                          )
+                              .whenComplete(() {
+                            emailText.clear();
+                            passwordText.clear();
+                            _services.dismissProgressDialog(context);
+                            _services.showMyDialog(
+                              context: context,
+                              title: "Save Delivery Boy",
+                              message: "Saved Successfully",
+                            );
+                          });
+                        },
+                        child: const Text(
+                          "Save",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import 'package:flutter/material.dart';
+
+import '../../firebase_services.dart';
+
+class CreateNewBoyWidget extends StatefulWidget {
   const CreateNewBoyWidget({super.key});
 
   @override
@@ -154,4 +332,4 @@ class _CreateNewBoyWidgetState extends State<CreateNewBoyWidget> {
                   ),
                 );
   }
-}
+}*/
