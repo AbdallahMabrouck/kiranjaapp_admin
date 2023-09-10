ece05c0dc88764e0df9a4a9a95728040fc3496ab
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-10 12:44:59 +0300

    modified firebase_services

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Kiranja - Admin Dashboard',
+      title: 'Kiranja - Admin',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
