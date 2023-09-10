b24d27867aa358f5996380445412039c545d35db
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-10 14:25:52 +0300

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:kiranjaapp_admin/screens/banners_screen.dart';
+import 'package:kiranjaapp_admin/screens/category_screen.dart';
+import 'package:kiranjaapp_admin/screens/delivery_boy_screen.dart';
+import 'package:kiranjaapp_admin/screens/home_screen.dart';
 import 'package:kiranjaapp_admin/screens/login_screen.dart';
+import 'package:kiranjaapp_admin/screens/notification_screen.dart';
+import 'package:kiranjaapp_admin/screens/orders_screen.dart';
 import 'package:kiranjaapp_admin/screens/splash_screen.dart';
+import 'package:kiranjaapp_admin/screens/vendor_screen.dart';
+import 'package:kiranjaapp_admin/widgets/admin_users.dart';
+import 'package:kiranjaapp_admin/widgets/settings.dart';
 import 'package:kiranjaapp_admin/widgets/side_menu.dart';
 import 'firebase_options.dart';
 
@@ -28,10 +37,19 @@ class MyApp extends StatelessWidget {
       initialRoute: SplashScreen.id,
       routes: {
         SplashScreen.id: (context) => const SplashScreen(),
+        HomeScreen.id: (context) => const HomeScreen(),
         LoginScreen.id: (context) => const LoginScreen(
               title: 'Kiranja - Admin',
             ),
         SideMenu.id: (context) => const SideMenu(),
+        BannersScreen.id: (context) => const BannersScreen(),
+        VendorsScreen.id: (context) => const VendorsScreen(),
+        CategoryScreen.id: (context) => const CategoryScreen(),
+        OrdersScreen.id: (context) => const OrdersScreen(),
+        NotificationsScreen.id: (context) => const NotificationsScreen(),
+        AdminUsers.id: (context) => const AdminUsers(),
+        Settings.id: (context) => const Settings(),
+        DeliveryBoyScreen.id: (context) => const DeliveryBoyScreen(),
       },
       builder: EasyLoading.init(),
     );
