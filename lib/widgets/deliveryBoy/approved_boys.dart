5ada1a8f121a0ef33cb944e16c60c7ce3e7b8944
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-10 17:05:38 +0300

    created approved boys widget

--- lib/widgets/deliveryBoy/approved_boys.dart
+++ lib/widgets/deliveryBoy/approved_boys.dart
@@ -1,5 +1,164 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
+
+import '../../firebase_services.dart';
+
+class ApprovedBoys extends StatefulWidget {
+  const ApprovedBoys({super.key});
+
+  @override
+  State<ApprovedBoys> createState() => _ApprovedBoysState();
+}
+
+class _ApprovedBoysState extends State<ApprovedBoys> {
+  final FirebaseService _services = FirebaseService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: StreamBuilder<QuerySnapshot>(
+        stream:
+            _services.boys.where("accVerified", isEqualTo: true).snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Something went wrong");
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          QuerySnapshot<Object?> snap = snapshot.data!;
+
+          if (snap.size == 0) {
+            return const Center(
+              child: Text("No Approved boys to list"),
+            );
+          }
+
+          return SingleChildScrollView(
+            child: DataTable(
+              showBottomBorder: true,
+              headingRowColor: MaterialStateProperty.all(Colors.grey.shade200),
+              columns: const <DataColumn>[
+                DataColumn(
+                  label: Text("Profile Pic"),
+                ),
+                DataColumn(
+                  label: Text("Name"),
+                ),
+                DataColumn(
+                  label: Text("Email"),
+                ),
+                DataColumn(
+                  label: Text("Mobile"),
+                ),
+                DataColumn(
+                  label: Text("Address"),
+                ),
+                DataColumn(
+                  label: Text("Action"),
+                ),
+              ],
+              rows: _boysList(snapshot.data!, context),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  List<DataRow> _boysList(QuerySnapshot snapshot, context) {
+    List<DataRow> newList = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+      return DataRow(cells: [
+        DataCell(
+          SizedBox(
+            width: 60,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.network(
+                data["imageUrl"],
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ),
+        DataCell(
+          Text(data["name"]),
+        ),
+        DataCell(
+          Text(data["email"]),
+        ),
+        DataCell(
+          Text(data["mobile"]),
+        ),
+        DataCell(
+          Text(data["address"]),
+        ),
+        DataCell(
+          FlutterSwitch(
+            activeText: "Approved",
+            activeColor: Colors.indigo,
+            inactiveColor: Colors.grey,
+            inactiveText: "Not Approved",
+            value: data["accVerified"],
+            valueFontSize: 10.0,
+            width: 110,
+            borderRadius: 30.0,
+            showOnOff: true,
+            onToggle: (val) {
+              _services.updateBoyStatus(
+                id: document.id,
+                context: context,
+                status: false,
+              );
+            },
+          ),
+        ),
+      ]);
+    }).toList();
+    return newList;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 import '../../firebase_services.dart';
 
@@ -72,12 +231,11 @@ class _ApprovedBoysState extends State<ApprovedBoys> {
   }
 
   List<DataRow> _boysList(QuerySnapshot snapshot, context) {
-    List<DataRow> newList = snapshot.docs.map((DocumentSnapshot document) {
-      print(document.data().length);
+    List<DataRow?> newList = snapshot.docs.map((DocumentSnapshot document) {
       if (document != null) {
         return DataRow(cells: [
           DataCell(
-            Container(
+            SizedBox(
                 width: 60,
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -99,14 +257,17 @@ class _ApprovedBoysState extends State<ApprovedBoys> {
           DataCell(
             Text(document.data()["address"]),
           ),
-          DataCell(FluterSwitch(
+          DataCell(
+            FlutterSwitch(
               activeText: "Approved",
-              InactiveText: "Not Approved",
+              activeColor: Colors.indigo,
+              inactiveColor: Colors.grey,
+              inactiveText: "Not Approved",
               value: document.data()["accVerified"],
               valueFontSize: 10.0,
               width: 110,
-              boarderRadius: 30.0,
-              shoOnOff: true,
+              borderRadius: 30.0,
+              showOnOff: true,
               onToggle: (val) {
                 _services.updateBoyStatus(
                     id: document.id, context: context, status: false);
@@ -117,3 +278,4 @@ class _ApprovedBoysState extends State<ApprovedBoys> {
     return newList;
   }
 }
+*/
