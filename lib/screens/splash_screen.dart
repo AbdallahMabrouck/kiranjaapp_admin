4b00630eb2a4e0de096a5bb040dd4666f44a03e3
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-13 21:59:13 +0300

    edited splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:kiranjaapp_admin/screens/home_screen.dart';
+import 'package:kiranjaapp_admin/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(const Duration(seconds: 3), () {
       FirebaseAuth.instance.authStateChanges().listen((User? user) {
         if (user == null) {
-          Navigator.pushReplacementNamed(context, HomeScreen.id);
+          Navigator.pushReplacementNamed(context, LoginScreen.id);
         } else {
           Navigator.pushReplacementNamed(context, HomeScreen.id);
         }
