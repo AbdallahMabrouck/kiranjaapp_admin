377b32115026f0d7becbde50c22b27ba150f4f15
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-15 19:59:53 +0300

    created category list widget

---
+++ lib/widgets/category/category_list_widget.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:kiranjaapp_admin/firebase_services.dart';
+
+class CategoryListWidget extends StatelessWidget {
+  const CategoryListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    FirebaseService _services = FirebaseService();
+
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: StreamBuilder(
+          stream: _services.category.snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text("Something went wrong"),
+              );
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return Wrap(
+              direction: Axis.horizontal,
+              children: snapshot.data.docs
+                  .map((DocumentSnapshot document) {})
+                  .toList(),
+            );
+          }),
+    );
+  }
+}
