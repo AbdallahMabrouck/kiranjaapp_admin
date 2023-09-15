2e6d78944a4437f2c83c125caeaa9f47143756b3
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-15 18:24:13 +0300

    modified banner widget

--- lib/screens/banners_screen.dart
+++ lib/screens/banners_screen.dart
@@ -1,30 +1,18 @@
-import 'dart:html';
-
-import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flutter/material.dart';
 import 'package:flutter_admin_scaffold/admin_scaffold.dart';
-import 'package:kiranjaapp_admin/widgets/sidebar.dart';
-import '../firebase_services.dart';
+
 import '../widgets/banner_widget.dart';
+import '../widgets/banners/banner_upload_widget.dart';
+import '../widgets/sidebar.dart';
 
-class BannersScreen extends StatefulWidget {
+class BannersScreen extends StatelessWidget {
   const BannersScreen({super.key});
-  static const String id = "banners-screen";
 
-  @override
-  State<BannersScreen> createState() => _BannersScreenState();
-}
-
-class _BannersScreenState extends State<BannersScreen> {
-  SideBarWidget sideBar = SideBarWidget();
-  final FirebaseService _services = FirebaseService();
-  final _fileNameTextController = TextEditingController();
-  bool _visible = false;
-  bool _imageSelected = true;
-  String? _url;
+  static const String id = "banner-screen";
 
   @override
   Widget build(BuildContext context) {
+    SideBarWidget sideBar = SideBarWidget();
     return AdminScaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -35,153 +23,32 @@ class _BannersScreenState extends State<BannersScreen> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      sideBar: sideBar.sideBarMenus(context, BannersScreen.id),
+      sideBar: sideBar.sideBarMenus(context, id),
       body: SingleChildScrollView(
         child: Container(
           alignment: Alignment.topLeft,
           padding: const EdgeInsets.all(10),
-          child: Column(
+          child: const Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
+              Text(
                 "Banners Screen",
                 style: TextStyle(fontSize: 36, fontWeight: FontWeight.w700),
               ),
-              const Text("Add/Delete Home Screen Banner Images"),
-              const Divider(
+              Text("Add/Delete Home Screen Banner Images"),
+              Divider(
                 thickness: 5,
               ),
               // Banners
-              const BannerWidget(),
-              const Divider(
+              BannerWidget(),
+              Divider(
                 thickness: 5,
               ),
-              Container(
-                color: Colors.grey,
-                width: MediaQuery.of(context).size.width,
-                height: 80,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 30),
-                  child: Row(
-                    children: [
-                      Visibility(
-                        visible: _visible,
-                        child: Row(
-                          children: [
-                            AbsorbPointer(
-                              absorbing: true,
-                              child: SizedBox(
-                                  width: 300,
-                                  height: 30,
-                                  child: TextField(
-                                    controller: _fileNameTextController,
-                                    decoration: const InputDecoration(
-                                        focusedBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(
-                                                color: Colors.black, width: 1)),
-                                        filled: true,
-                                        fillColor: Colors.white,
-                                        hintText: "No Image Selected",
-                                        border: OutlineInputBorder(),
-                                        contentPadding:
-                                            EdgeInsets.only(left: 20)),
-                                  )),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                uploadStorage();
-                              },
-                              child: const Text(
-                                "Upload Image",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            AbsorbPointer(
-                              absorbing: _imageSelected,
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  _services
-                                      .uploadBannerImageToDb(_url)
-                                      .then((downloadUrl) {
-                                    _services.showMyDialog(
-                                        context: context,
-                                        title: "New Banner Image",
-                                        message:
-                                            "Saved Banner Image Successifully");
-                                  });
-                                },
-                                child: const Text(
-                                  "Save Image",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Visibility(
-                        visible: _visible,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              _visible = true;
-                            });
-                          },
-                          child: const Text(
-                            "Add New Banner",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
+              BannerUploadWidget()
             ],
           ),
         ),
       ),
     );
-  }
-
-  void uploadImage({required Function(File file) onSelected}) {
-    FileUploadInputElement uploadInput = FileUploadInputElement()
-      ..accept = "image/*";
-    // it will upload only image
-    uploadInput.click();
-    uploadInput.onChange.listen((event) {
-      final file = uploadInput.files!.first;
-      final reader = FileReader();
-      reader.readAsDataUrl(file);
-      reader.onLoadEnd.listen((event) {
-        onSelected(file);
-      });
-    });
-    // selected image
-  }
-
-  void uploadStorage() {
-    // upload selected image to firebase storage
-    final dateTime = DateTime.now();
-    final path = "bannerImage/${dateTime.microsecondsSinceEpoch}";
-    uploadImage(onSelected: (file) {
-      setState(() {
-        _fileNameTextController.text = file.name;
-        _imageSelected = false;
-        _url = path;
-      });
-      firebase_storage.FirebaseStorage.instance
-          .ref(path)
-          .putBlob(file)
-          .then((snapshot) {
-        // Get the download URL after the file is uploaded
-        snapshot.ref.getDownloadURL().then((downloadUrl) {
-          // Handle the download URL
-        });
-      });
-    });
   }
 }
